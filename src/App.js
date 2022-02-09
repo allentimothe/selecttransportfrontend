93cec484217af00b1c18947ec59f402edb94c8cf
Allen Timothe <allen.timothe@gmail.com>
2022-02-08 20:02:11 -0500

    Client Set Up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import Header1 from "./components/Header/Header1";
+import Footer from "./components/Footer/Footer";
 import './App.css';
 
+useEffect(() => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setState((prevState) => ({
+        ...prevState,
+        user,
+      }));
+    } else {
+      setState((prevState) => ({
+        ...prevState,
+        user: null,
+      }));
+    }
+  });
+  async function getContacts() {
+    const contact = await fetchReviews();
+    setContactsState({ contact });
+  }
+  getContacts();
+}, []);
+
+async function handleAdd(formInputs) {
+  try {
+    const contact = await createContact(formInputs);
+    setContactState({ contact });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleDelete(reviewId) {
+  try {
+    const contact = await deleteContact(reviewId);
+    setContactState({ contact });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleUpdate(formInputs) {
+  try {
+    const contact = await updateContact(formInputs);
+    setContactState({ contact });
+  } catch (error) {
+    console.log(error);
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header user={state.user} />
+
+        <main>
+        </main>
+
+        <Footer />
+      </div>
     </div>
   );
 }
